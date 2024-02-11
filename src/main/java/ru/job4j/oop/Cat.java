f5994063ac2c0dff5b261b607fc6b1960c68f30d
Kife22 <Kife22@mail.ru>
2024-02-11 15:00:23 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,19 +3,18 @@ package ru.job4j.oop;
 public class Cat {
 
     private String name;
+    private String food;
 
     public void giveNick(String nick) {
         this.name = nick;
     }
 
-    private String food;
+    public void eat(String meat) {
+        this.food = meat;
+    }
 
     public void show() {
         System.out.println(this.name + " eated a " + this.food);
-    }
-
-    public void eat(String meat) {
-        this.food = meat;
     }
 
     public static void main(String[] args) {
