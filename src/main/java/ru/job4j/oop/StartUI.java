2ccab88b710b9215cbfaac3632e17c6d859a7899
Kife22 <Kife22@mail.ru>
2024-02-20 19:31:45 +0300

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/oop/StartUI.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+import ru.job4j.tracker.Item;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime createdDate = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String formattedDate = createdDate.format(formatter);
+        System.out.println("Item created on: " + formattedDate);
+    }
+}
