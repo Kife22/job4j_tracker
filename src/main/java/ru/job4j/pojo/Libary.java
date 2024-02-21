17b6eadccefd593366d3defa157a6bc129cf152b
Kife22 <Kife22@mail.ru>
2024-02-21 20:52:28 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Libary.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Libary {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean cod", 464);
+        Book book2 = new Book("1984", 352);
+        Book book3 = new Book("Pride and Prejudice", 360);
+        Book book4 = new Book("The catcher in the rye", 224);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            System.out.println("Length book " + book.getName() + " - " + book.getNumber());
+        }
+        System.out.println("Show only \"Clean cod\"");
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            if (book.getName() == "Clean cod") {
+                System.out.println("Length book " + book.getName() + " - " + book.getNumber());
+            }
+        }
+        System.out.println("Swap");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (Book book : books) {
+            System.out.println("Length of the book " + book.getName() + " - " + book.getNumber());
+        }
+    }
+}
