795388b53c981683d43e3ce3908862f2c23485b9
Kife22 <Kife22@mail.ru>
2024-02-22 19:13:32 +0300

    8. Date. Отображение даты. [#310017 #482001]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,28 @@
 package ru.job4j.tracker;
-import com.sun.source.tree.IdentifierTree;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Item {
-
-    private final LocalDateTime created = LocalDateTime.now();
-
     private int id;
+
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
     }
 
     public void setId(int id) {
@@ -29,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
