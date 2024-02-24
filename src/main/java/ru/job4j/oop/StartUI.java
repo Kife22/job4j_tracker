ab20922e0e66d7dfec045dc48435a468319092d5
Kife22 <Kife22@mail.ru>
2024-02-24 15:17:32 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/oop/StartUI.java
+++ src/main/java/ru/job4j/oop/StartUI.java
@@ -9,9 +9,11 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
+        Item itemSecond = new Item();
         LocalDateTime createdDate = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String formattedDate = createdDate.format(formatter);
         System.out.println("Item created on: " + formattedDate);
+        System.out.println(itemSecond);
     }
 }
