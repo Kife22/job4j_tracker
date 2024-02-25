6a9a4fd49ac05828c258fd745e80e881d3604d6b
Kife22 <Kife22@mail.ru>
2024-02-25 18:39:29 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College2.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class College2 {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = freshman;
+        //Относится к up casting//
+    }
+}
