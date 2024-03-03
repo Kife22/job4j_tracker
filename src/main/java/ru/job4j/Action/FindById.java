3646f76230303970d60480b890964b67fd929bf9
Kife22 <Kife22@mail.ru>
2024-03-03 17:44:36 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/Action/FindById.java
+++ src/main/java/ru/job4j/Action/FindById.java
@@ -1,9 +1,11 @@
-package ru.job4j.tracker;
+package ru.job4j.Action;
 
-public class FindByIdAction implements UserAction {
+import ru.job4j.tracker.*;
+
+public class FindById implements UserAction {
     private final Output output;
 
-    public FindByIdAction(Output output) {
+    public FindById(Output output) {
         this.output = output;
     }
 
