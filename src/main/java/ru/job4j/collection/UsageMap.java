a22ef47e49ed42189aecf494c2a41015a9de0bcc
Kife22 <Kife22@mail.ru>
2024-04-07 18:12:55 +0900

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("kife22@mail.ru", "Kirill Gutnik");
+        map.put("summerset216@mail.ru", "Artem Patek");
+        map.put("kife22@mail.ru", "Kirill Gutnik");
         for (String maps : map.keySet()) {
             String value = map.get(maps);
             System.out.println(maps + " = " + value);
