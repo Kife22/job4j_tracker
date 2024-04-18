6982ff90e478f3ab37e741f16600e1ddfb850590
Kife22 <Kife22@mail.ru>
2024-04-19 03:33:13 +0900

    0. Сортировка [#10096]

---
+++ src/main/java/ru/job4j/collection/Item.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+public class Item implements Comparable<Item> {
+    private String name;
+
+    private int priority;
+
+    public Item(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "name='" + name + '\''
+                + ", priority=" + priority
+                + '}';
+    }
+
+    @Override
+    public int compareTo(Item anothe) {
+        return Integer.compare(priority, anothe.priority);
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ItemDescByNameTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItemDescByNameTest {
+
+    @Test
+    public void testSortDescendingByName() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("A", 3));
+        items.add(new Item("B", 1));
+        items.add(new Item("C", 2));
+        List<Item> expected = new ArrayList<>(items);
+        Collections.sort(expected, new ItemDescByName());
+        Collections.sort(items, new ItemDescByName()); // Сортировка по убыванию имени
+        assertEquals(expected, items);
+    }
+}
